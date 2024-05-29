c8c26b53e2ef1e12a66bd21900fc004c4450de70
Peter Doohan <peterdoohan0@gmail.com>
2024-05-29 09:05:53 +0000

    added requirements file and commented out old KS4 code to be replaced with spike interface

--- run_kilosort.py
+++ run_kilosort.py
@@ -4,9 +4,6 @@
 import numpy as np
 from pathlib import Path
 from datetime import datetime as dt
-from kilosort import io
-from kilosort import run_kilosort
-from spikeinterface import extractors as se
 
 
 # %% Global variables
@@ -26,29 +23,29 @@ TROUBLESHOOT_SESSION = "../data/raw_data/ephys/mEC_6/2024-02-20_12-55-26/Record 
 
 
 # %% Functions
-def run_kilosort4(ephys_path, specified_output_folder=False):
-    # define output folder:
-    subject, collection_datetime = Path(ephys_path).parts[-2:]
-    sortname = specified_output_folder if specified_output_folder else dt.now().isoformat()
-    output_folder = Path(KILOSORT_PATH) / subject / collection_datetime / sortname
-    output_folder.mkdir(parents=True, exist_ok=True)
-    # Load data
-    raw_AP = se.read_openephys(ephys_path, stream_id="0")  # stream_id 0 is AP data, 1 is LFP data
-    filename, N, c, s, fs, probe_path = io.spikeinterface_to_binary(
-        raw_AP,
-        output_folder,
-        data_name="data.bin",
-        dtype=np.int16,
-        chunksize=60000,
-        export_probe=True,
-        probe_name="probe.prb",
-    )
-    settings = {"fs": fs, "n_chan_bin": c}
-    probe = io.load_probe(probe_path)
-    ops, st, clu, tF, Wall, similar_templates, is_ref, est_contam_rate = run_kilosort(
-        settings=settings,
-        probe=probe,
-        filename=filename,
-        results_dir=output_folder,
-    )
-    return
+# def run_kilosort4(ephys_path, specified_output_folder=False):
+#     # define output folder:
+#     subject, collection_datetime = Path(ephys_path).parts[-2:]
+#     sortname = specified_output_folder if specified_output_folder else dt.now().isoformat()
+#     output_folder = Path(KILOSORT_PATH) / subject / collection_datetime / sortname
+#     output_folder.mkdir(parents=True, exist_ok=True)
+#     # Load data
+#     raw_AP = se.read_openephys(ephys_path, stream_id="0")  # stream_id 0 is AP data, 1 is LFP data
+#     filename, N, c, s, fs, probe_path = io.spikeinterface_to_binary(
+#         raw_AP,
+#         output_folder,
+#         data_name="data.bin",
+#         dtype=np.int16,
+#         chunksize=60000,
+#         export_probe=True,
+#         probe_name="probe.prb",
+#     )
+#     settings = {"fs": fs, "n_chan_bin": c}
+#     probe = io.load_probe(probe_path)
+#     ops, st, clu, tF, Wall, similar_templates, is_ref, est_contam_rate = run_kilosort(
+#         settings=settings,
+#         probe=probe,
+#         filename=filename,
+#         results_dir=output_folder,
+#     )
+#     return
